b78b8044eca8af269da577a16284f0bc1c0eebd2
NovaPlay <marcosperezvindel128@gmail.com>
2026-03-25 00:24:54 +0100

    Add HolaMundo.cs with basic Hello World program

---
+++ HolaMundo.cs
@@ -0,0 +1,15 @@
+using System; // 1. Cargamos las herramientas básicas
+
+class Program
+{
+    static void Main()
+    {
+        // Esto escribe el texto en la pantalla
+        Console.WriteLine("¡Hola Mundo! Este es mi primer paso para EEUU.");
+
+        // Esto hace que la ventana no se cierre sola. Se queda esperando a que pulses una tecla
+        Console.WriteLine("Pulsa cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
+
