39440265c922bb2a0a1b98d7eed1f6a7d9ea5ce1
NovaPlaycode <marcosperezvindel128@gmail.com>
2026-03-25 18:39:51 +0100

    Agregar archivos de proyecto.

---
+++ MiPrimerCodigo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // 1. Cargamos las herramientas básicas
+
+class Program
+{
+    static void Main()
+    {
+        // 2. Esto escribe el texto en la pantalla
+        Console.WriteLine("¡Hola Mundo! Este es mi primer paso para USA.");
+
+        // 3. Esto hace que la ventana no se cierre sola. Se queda esperando a que pulses una tecla
+        Console.WriteLine("Pulsa cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
+
---
+++ MiPrimerCodigo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("MiPrimerCodigo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("MiPrimerCodigo")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("89db1032-6998-4c1a-ad77-c31fb07efa03")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
